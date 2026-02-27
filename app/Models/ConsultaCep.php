5886cad2bca882dfbf947f0a40c437b281117f1a
dev-paulocoelho-admim <paulomedinabr01@gmail.com>
2026-02-27 12:07:11 -0300

    refactor: add HasFactory trait to ConsultaCep model

--- app/Models/ConsultaCep.php
+++ app/Models/ConsultaCep.php
@@ -2,11 +2,14 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class ConsultaCep extends Model
 {
+    use HasFactory;
+
     /**
      * The table associated with the model.
      * @var string
